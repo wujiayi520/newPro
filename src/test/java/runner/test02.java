a5c83a52149d895284e35775d81ee70eca9999ff
manbufanhuajie <manbufanhuajie@192.168.0.3>
2020-07-09 19:27:32 +0800

    y

--- src/test/java/runner/test02.java
+++ src/test/java/runner/test02.java
@@ -1,11 +1,30 @@
 package runner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class test02 {
 	@Test
 	public void test() {
 		System.out.println("+++++++"+"这是另一个测试任务");
+	}
+	
+	@Test
+	public void send() {
+		List<String> list=new ArrayList<String>();
+		list.add("name");
+		list.add("age");
+		list.add("凤凰");
+		Iterator<String> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			String next = iterator.next();
+			System.out.println(next);
+			
+		}
 	}
 
 }
