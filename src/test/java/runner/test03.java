a1a8eff588e11f2b49139f434a3ad539f568fbd4
manbufanhuajie <manbufanhuajie@192.168.0.3>
2020-07-09 19:04:17 +0800

    123

---
+++ src/test/java/runner/test03.java
@@ -0,0 +1,22 @@
+package runner;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class test03 {
+	@Test
+	public void test() {
+		int a=1;
+		int b=2;
+		System.out.println("---开始错误用例-------");
+		System.out.println("---开始错误用例-------");
+
+		System.out.println(b/a);
+		
+	}
+	@Test
+	public void test2() {
+		System.out.println("sjdsnjvndfv");
+	}
+
+}
